18e8396495bf6a02825c8d65fefd9388c12f18e5
RoiMP <roimartinezportela@gmail.com>
2019-04-23 21:59:11 +0200

    Cambio en el Cliente by Roi
    He separado la función de inicialización y la de cerrar sesión para
    poder tratrlas más cómodamente en el resto de funciones (y no tener que
    poner lo mismo en todas)

--- Proyecto/src/proyecto/Cliente.java
+++ Proyecto/src/proyecto/Cliente.java
@@ -6,35 +6,43 @@ import java.util.Scanner;
 public class Cliente {
 
 	static final String JDBC_DRIVER = "com.mysql.jdb.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/TaquillaVirtual?useSSL=false";
-	
+	static final String DB_URL = "jdbc:mysql://localhost/Proyecto?useSSL=false"; //TODO como huevá se llame la BD
+
 	static final String USER = "cliente";
 	static final String PASSWORD = "27632298";
-	
-	public static void main(String[] args) {
 
-		Connection conn = null;
-		Statement stmt = null;
-		Scanner sc = new Scanner(System.in);
-		
-		try {
-			Class.forName(JDBC_DRIVER);
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/Proyecto?useSSL=false&autoReconnect=true",USER,PASSWORD);
-			stmt = conn.createStatement();
+	private static Connection conn = null;
+	private static Statement stmt = null;
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		/* INSERTAR INTERFAZ Y PRUEBAS A REALIZAR
-		 * */
-		System.out.println("BIENVENIDO AL ");
-		
-		
-		
-		
-		
-		
+	/**
+	 * Inicio de la conexión con la BD SQL
+	 * 
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	void init() throws SQLException, ClassNotFoundException {
+		Class.forName(JDBC_DRIVER);
+
+		System.out.println("Connecting to database...");
+		conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+		System.out.println("Creating statement...");
+		stmt = conn.createStatement();
+
+		return;
+	}
+
+	/**
+	 * Cierre de conexión con la BD SQL
+	 * 
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	void close() throws SQLException {
+		stmt.close();
+		conn.close();
+
+		return;
 	}
 
 }
